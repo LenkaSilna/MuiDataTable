3b091e7eddc8bc64cbfaaae1c3f3ddb44ccba958
Lenka Silná <lenka.baricova@post.cz>
2021-07-06 12:10:16 +0200

    build

---
+++ build/precache-manifest.d358a5a97124cb33f60ea1d0ebac9b86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c31adffe7327555176cf4965c6fb7aa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4576d03e241dd674f6ba",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "a9ccef86dc81b378aeac",
+    "url": "/static/js/2.77b1e7e4.chunk.js"
+  },
+  {
+    "revision": "4576d03e241dd674f6ba",
+    "url": "/static/js/main.6545b48d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
